a0a7b6c1660b359cda9d750bafe06a2215399605
cjabs <cjabs97@gmail.com>
2019-02-06 18:52:41 -0800

    Renamed Movement script to PlayerMovement

--- 161SpaceInvaders/Assets/Scripts/PlayerMovement.cs
+++ 161SpaceInvaders/Assets/Scripts/PlayerMovement.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Movement : MonoBehaviour
+public class PlayerMovement : MonoBehaviour
 {
     private Rigidbody2D m_rgb2d;
     public int speed = 10;
