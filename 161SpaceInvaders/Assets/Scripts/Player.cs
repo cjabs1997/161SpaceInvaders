f4c2f1ede0b6f4c7d75daecc62ca64cfa2d4d4cc
cjabs <cjabs97@gmail.com>
2019-02-06 19:41:10 -0800

    Added shooting to player prefab and made player script

---
+++ 161SpaceInvaders/Assets/Scripts/Player.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    private Rigidbody2D m_rgb2d;
+    private Transform m_transform;
+    private Shoot m_shoot;
+
+    public float shootSpeed = 5.0f;
+    public float moveSpeed = 10.0f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        m_rgb2d = this.GetComponent<Rigidbody2D>();
+        m_transform = this.GetComponent<Transform>();
+        m_shoot = this.GetComponent<Shoot>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Move();
+
+        if (Input.GetKeyDown(KeyCode.Space) || Input.GetKeyDown(KeyCode.Mouse0))
+        {
+            Shoot();
+        }
+    }
+
+    private void Move()
+    {
+        float movementModifier = Input.GetAxisRaw("Horizontal");
+
+        Vector2 currentVelocity = m_rgb2d.velocity;
+        m_rgb2d.velocity = new Vector2(movementModifier * moveSpeed, m_rgb2d.velocity.y);
+    }
+
+    private void Shoot()
+    {
+        Vector2 bulletSpawn = new Vector2(m_transform.position.x, m_transform.position.y + 0.5f);
+        m_shoot.shoot(m_transform.position, Vector2.up, shootSpeed);
+    }
+
+}
--- 161SpaceInvaders/Assets/Scripts/Shoot.cs
+++ 161SpaceInvaders/Assets/Scripts/Shoot.cs
@@ -6,7 +6,7 @@ public class Shoot : MonoBehaviour
 {
     public GameObject bullet;
 
-    void shoot(Vector2 startPos, Vector2 direction, float speed)
+    public void shoot(Vector2 startPos, Vector2 direction, float speed)
     {
     	GameObject newBullet = Instantiate(bullet, startPos, Quaternion.identity);
     	newBullet.GetComponent<Rigidbody2D>().velocity = direction * speed;
