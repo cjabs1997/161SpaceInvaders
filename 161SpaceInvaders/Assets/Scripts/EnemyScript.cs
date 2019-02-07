20c68b2d8559fd5093e4ffaddc94247ba2467bbf
rygreen <rygreen99@gmail.com>
2019-02-06 20:54:56 -0800

    Made Enemy movement
    :^)

--- 161SpaceInvaders/Assets/Scripts/EnemyScript.cs
+++ 161SpaceInvaders/Assets/Scripts/EnemyScript.cs
@@ -1,18 +1,38 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Events;
+
+public class EnemyEvent : UnityEvent{}
 
 public class EnemyScript : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+	public EnemyEvent OnWallCollide = new EnemyEvent();
+	public float moveSpeed;
+	private Rigidbody2D m_rigidbody;
 
-    // Update is called once per frame
-    void Update()
+	void Awake()
+	{
+		m_rigidbody = this.GetComponent<Rigidbody2D>();
+	}
+
+	void Start()
+	{
+		m_rigidbody.velocity = new Vector2(moveSpeed, m_rigidbody.velocity.y);
+		OnWallCollide.AddListener(dropNswap);
+	}
+
+	void dropNswap()
+	{
+		transform.position = new Vector3(transform.position.x, transform.position.y-1, transform.position.z);
+		m_rigidbody.velocity = new Vector2(-m_rigidbody.velocity.x, m_rigidbody.velocity.y);
+	}
+
+    void OnCollisionEnter2D(Collision2D collision)
     {
-        
+    	if(collision.collider.gameObject.CompareTag("Wall"))
+    	{
+    		OnWallCollide.Invoke();
+    	}
     }
 }
