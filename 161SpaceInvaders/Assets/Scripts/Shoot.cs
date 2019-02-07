cb70d9749d8d892451cf21bedd5ebc8323f28457
rygreen <rygreen99@gmail.com>
2019-02-06 18:48:52 -0800

    TMP and Shoot script
    imported tmp and created a general shooting script :^)

---
+++ 161SpaceInvaders/Assets/Scripts/Shoot.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Shoot : MonoBehaviour
+{
+    public GameObject bullet;
+
+    void shoot(Vector2 startPos, Vector2 direction, float speed)
+    {
+    	GameObject newBullet = Instantiate(bullet, startPos, Quaternion.identity);
+    	newBullet.GetComponent<Rigidbody2D>().velocity = direction * speed;
+    }
+}
