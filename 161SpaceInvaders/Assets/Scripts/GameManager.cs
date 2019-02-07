51ef2461a2367f9d88421e94e6b07553fd2d3a60
cjabs <cjabs97@gmail.com>
2019-02-06 21:07:07 -0800

    Added dropandswap to gamemanager script

--- 161SpaceInvaders/Assets/Scripts/GameManager.cs
+++ 161SpaceInvaders/Assets/Scripts/GameManager.cs
@@ -35,8 +35,24 @@ public class GameManager : MonoBehaviour
 
                 newEnemy.GetComponent<Shoot>().bullet = enemyBulletPrefab;
                 newEnemy.GetComponent<EnemyScript>().moveSpeed = 2.0f;
+                newEnemy.GetComponent<EnemyScript>().OnWallCollide.AddListener(DropAndSwap);
 
                 enemyGrid[x].Add(newEnemy);
+            }
+        }
+    }
+
+    void DropAndSwap()
+    {
+        for (int x = 0; x < 11; x++)
+        {
+            for (int y = 0; y < 5; y++)
+            {
+                if(enemyGrid[x][y] != null)
+                {
+                    enemyGrid[x][y].GetComponent<Transform>().position = new Vector2(enemyGrid[x][y].GetComponent<Transform>().position.x, enemyGrid[x][y].GetComponent<Transform>().position.y-1);
+                    enemyGrid[x][y].GetComponent<Rigidbody2D>().velocity = new Vector2(-enemyGrid[x][y].GetComponent<Rigidbody2D>().velocity.x, 0.0f);
+                }
             }
         }
     }
