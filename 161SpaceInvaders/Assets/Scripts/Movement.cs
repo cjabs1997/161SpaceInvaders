143d89f9c3f4464f1f3798da8ef59a263b76767a
cjabs <cjabs97@gmail.com>
2019-02-06 18:38:01 -0800

    Made movement script

---
+++ 161SpaceInvaders/Assets/Scripts/Movement.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movement : MonoBehaviour
+{
+    private Rigidbody2D m_rgb2d;
+    public int speed = 10;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        m_rgb2d = this.GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Move();
+    }
+
+    private void Move()
+    {
+        float movementModifier = Input.GetAxisRaw("Horizontal");
+
+        Vector2 currentVelocity = m_rgb2d.velocity;
+        m_rgb2d.velocity = new Vector2(movementModifier * speed, m_rgb2d.velocity.y);
+    }
+}
